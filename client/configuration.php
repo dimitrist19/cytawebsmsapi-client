0d8955634485cc903de01e83751f9c127a847671
Dimitris T <56406468+dimitrist19@users.noreply.github.com>
2022-01-06 13:15:05 +0200

    Create configuration.php

---
+++ client/configuration.php
@@ -0,0 +1,10 @@
+<?php
+
+$websmsapi_config = array(
+    "version" => "1.0", //Enter Version for WebSmsApi Request (Default Value: 1.0)
+    "username" => "", //Enter Cyta Web Sms Sender Username (Obtain from MyCyta Account)
+    "apiKey" => "", // Enter Cyta Web Sms Sender Api Key (Obtain from MyCyta Account)
+    "language" => "el" // Values: el/en
+);
+?>
+
